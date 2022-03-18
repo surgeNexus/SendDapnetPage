3258d9cba5809506274d1fab82ee6035b0b3616b
Jordan Webb <jordan@k4avg.com>
2022-03-18 13:25:03 -0400

    initial commit

--- functionTest.js
+++ functionTest.js
@@ -3,4 +3,4 @@ const sendMessage = require('./send');
 // If emergency is blank, default is false. If any value is input in the fourth arg, will send true.
 
 // dest/msg/txgroup,emergency
-sendMessage("CHANGEME", "CHANGEME: this is a test", "us-all");
+sendMessage("k4avg", "k4avg: this is a test", "us-tn");
